755fed7a17f706d3530d95049d929b389291aec9
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2023-01-18 19:02:34 +0100

    Creo los endpoints para obtener todos los usuarios, mis matches y un usuario en concreto.

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,53 @@
 const functions = require("firebase-functions");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+
+// Modulo admin de Firebase
+const admin = require("firebase-admin")
+
+// Inicializamos la aplicacion
+admin.initializeApp()
+
+// Cargo el modulo de la base de datos
+const db = admin.firestore()
+
+ 
+
+
+// Obtener todos los usuarios
+exports.getUsers = functions.https.onRequest(async (request, response) => {
+    const usersDocs =  await db.collection('users').get()
+    console.log('userdocs', usersDocs)
+    console.log('is Undefined?', usersDocs.docs)
+    const userData = usersDocs.docs.map((eachDoc)=>{
+      return eachDoc.data()
+    })
+    console.log('userData', userData)
+    response.send(userData);
 });
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+// OBtener solo un usuario
+
+exports.getUser = functions.https.onRequest(async (request, response) => {
+
+
+  const uid = request.query.uid
+  console.log('uid', uid)
+  const user = await (await db.collection('users').doc(uid).get()).data()
+
+  response.send(user);
 });
+
+
+// Endpoint que me devuelva todos mis matches
+
+exports.getMyMatches = functions.https.onRequest(async (request, response) => {
+
+
+  const uid = request.query.uid
+  console.log('uid', uid)
+  const arrayOfDocs = await db.collection('users').doc(uid).collection('weLikeEachOther').get()
+  const arrayOfData = arrayOfDocs.docs.map((eachDoc)=> eachDoc.data())
+
+  response.send(arrayOfData);
+});
+
